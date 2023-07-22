aeabf69e8d3fba9329e207c50654c68df7635470
Anton <anton.smith.game@gmail.com>
2023-07-22 16:26:21 +0200

    Börja på orm

--- Snake.py
+++ Snake.py
@@ -4,16 +4,31 @@ import pygame
 
 WINDOW_HEIGHT = 640
 WINDOW_WIDTH = 640
+BLOCK_SIZE = 32
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+class Snake:
+    def __init__(self):
+        self.body = [(40,40),(40,60),(40,80)]
+        self.size = (20,20)
+        self.direction = 1
+    
+    def draw(self,screen):
+        for segment in self.body:
+            pygame.draw.rect(screen, (0,255,0),(segment[0],segment[1], BLOCK_SIZE, BLOCK_SIZE))
+    
+    def move(self):
+        for segment in self.body:
+
 
 running = True
-
+snake = Snake()
 while running:
+    # Close game window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    pygame.draw.circle(screen,(255,0,0),(320,320),10)
+    snake.draw(screen)
     pygame.display.update()
