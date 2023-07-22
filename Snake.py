26fadaee94850a20234ae7ecfc9cab513f372a21
Anton <anton.smith.game@gmail.com>
2023-07-22 16:05:57 +0200

    Fungerande fönster

--- Snake.py
+++ Snake.py
@@ -4,3 +4,16 @@ import pygame
 
 WINDOW_HEIGHT = 640
 WINDOW_WIDTH = 640
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.draw.circle(screen,(255,0,0),(320,320),10)
+    pygame.display.update()
